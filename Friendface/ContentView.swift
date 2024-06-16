b66f654c35435560eaa09d45a6ddfc49c3654cf9
Víctor Ávila <victoraavila2000@gmail.com>
2024-06-16 18:44:35 -0300

    Initial Commit

---
+++ Friendface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by Víctor Ávila on 16/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Víctor Ávila on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
